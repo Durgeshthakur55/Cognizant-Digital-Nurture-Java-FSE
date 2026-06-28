2ee55be717b119bd19ad89b5f92ac957061bf8e0
Durgesh Pratap Singh <thakurdurgeshpratap555@gmail.com>
2026-06-28 17:15:00 +0530

    Completed Exercise 2: Simple Calculator

---
+++ Simple_Calculator/Simple_Calc.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class Simple_Calc {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Choose an operation (+, -, *, /): ");
+        char operation = scanner.next().charAt(0);
+
+        if (operation == '+') {
+            System.out.println("Result: " + (num1 + num2));
+        } else if (operation == '-') {
+            System.out.println("Result: " + (num1 - num2));
+        } else if (operation == '*') {
+            System.out.println("Result: " + (num1 * num2));
+        } else if (operation == '/') {
+            System.out.println("Result: " + (num1 / num2));
+        } else {
+            System.out.println("Invalid operation selected.");
+        }
+
+        scanner.close();
+    }
+}
+
+    
+
