07d2305456e221b36cfb2c2e982ee65eaccc3ca7
Durgesh Pratap Singh <thakurdurgeshpratap555@gmail.com>
2026-07-22 17:30:12 +0530

    Exercise 9: Spring Boot REST Application with H2 Database

--- spring_core_maven/src/main/java/com/library/repository/BookRepository.java
+++ spring_core_maven/src/main/java/com/library/repository/BookRepository.java
@@ -1,7 +1,8 @@
 package com.library.repository;
 
-import com.library.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.library.entity.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
