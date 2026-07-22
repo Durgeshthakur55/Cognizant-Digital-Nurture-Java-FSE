96cbd76f7441b0337465a97fb8b16ac6480feafe
Durgesh Pratap Singh <thakurdurgeshpratap555@gmail.com>
2026-07-22 16:39:42 +0530

    Exercise 5: Configuring the Spring IoC Container

--- spring_core_maven/src/main/java/com/library/repository/BookRepository.java
+++ spring_core_maven/src/main/java/com/library/repository/BookRepository.java
@@ -2,6 +2,6 @@ package com.library.repository;
 
 public class BookRepository {
     public void fetchBooks() {
-        System.out.println("Fetching book records from the database...");
+        System.out.println("[BookRepository] Fetching books from database via Spring IoC Container...");
     }
 }
