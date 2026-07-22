d0d2d86278f65f992f0548a8b60b365e6c9e596a
Durgesh Pratap Singh <thakurdurgeshpratap555@gmail.com>
2026-07-22 16:45:31 +0530

    Exercise 7: Implementing Constructor and Setter Injection

--- spring_core_maven/src/main/java/com/library/service/BookService.java
+++ spring_core_maven/src/main/java/com/library/service/BookService.java
@@ -4,13 +4,20 @@ import com.library.repository.BookRepository;
 
 public class BookService {
     private BookRepository bookRepository;
+    private String serviceName;
+
+    public BookService() {}
+
+    public BookService(String serviceName) {
+        this.serviceName = serviceName;
+    }
 
     public void setBookRepository(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
 
     public void manageBooks() {
-        System.out.println("BookService active. Managing library inventory...");
+        System.out.println("[" + (serviceName != null ? serviceName : "BookService") + "] Managing library inventory...");
         bookRepository.fetchBooks();
     }
 }
