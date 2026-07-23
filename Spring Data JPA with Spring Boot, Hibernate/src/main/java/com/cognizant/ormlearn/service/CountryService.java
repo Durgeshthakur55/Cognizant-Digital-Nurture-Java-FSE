bd940b1d81b89daab39674e35e386731754b9744
Durgesh Pratap Singh <thakurdurgeshpratap555@gmail.com>
2026-07-23 23:14:47 +0530

    Implement services for managing Country

--- Spring Data JPA with Spring Boot, Hibernate/src/main/java/com/cognizant/ormlearn/service/CountryService.java
+++ Spring Data JPA with Spring Boot, Hibernate/src/main/java/com/cognizant/ormlearn/service/CountryService.java
@@ -1,6 +1,7 @@
 package com.cognizant.ormlearn.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,5 +19,36 @@ public class CountryService {
     @Transactional
     public List<Country> getAllCountries() {
         return countryRepository.findAll();
+    }
+
+    @Transactional
+    public Country findCountryByCode(String countryCode) {
+        Optional<Country> result = countryRepository.findById(countryCode);
+        return result.orElse(null);
+    }
+
+    @Transactional
+    public void addCountry(Country country) {
+        countryRepository.save(country);
+    }
+
+    @Transactional
+    public void updateCountry(String code, String name) {
+        Optional<Country> optionalCountry = countryRepository.findById(code);
+        if (optionalCountry.isPresent()) {
+            Country country = optionalCountry.get();
+            country.setName(name);
+            countryRepository.save(country);
+        }
+    }
+
+    @Transactional
+    public void deleteCountry(String code) {
+        countryRepository.deleteById(code);
+    }
+
+    @Transactional
+    public List<Country> getCountriesByPartialName(String name) {
+        return countryRepository.findByNameContaining(name);
     }
 }
